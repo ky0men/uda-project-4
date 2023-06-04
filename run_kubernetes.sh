ae96fde3c73d59ef99294b832c1dc2349f08461c
Pham Phu Dien <phamphudien@gmail.com>
2023-06-04 13:17:20 +0700

    push source after run local complete

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,17 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerPath=dienpp/prj4
+dockerPath=dienpp/prj4:ver1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deploy kube-prj4 --image="$docker_path"
+kubectl create deploy uda-prj4-kube --image=$dockerPath --port=8000
 
 
 # Step 3:
 # List kubernetes pods
-kubectl get pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/kube-prj4 8000:80
+kubectl port-forward deployment/uda-prj4-kube 8000:80
